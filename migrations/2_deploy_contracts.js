c146633a1bda18277585141eda8528e5a3890790
Raymond Yeh <ray@whitefables.com>
2018-01-16 17:32:46 +0800

    Removed zepplin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const MerkleProof = artifacts.require("zeppelin-solidity/contracts/MerkleProof.sol");
 const CertificateStore = artifacts.require("./CertificateStore.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MerkleProof);
-  deployer.link(MerkleProof, CertificateStore);
   deployer.deploy(CertificateStore, "www.tech.gov.sg", "Government Technology Agency of Singapore (GovTech)");
 };
