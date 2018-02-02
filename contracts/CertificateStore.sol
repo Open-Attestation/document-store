ba428099b67abd67710ff403223b1cc0dbb02f63
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-02-02 14:07:34 +0800

    Major cleanup of tests

--- contracts/CertificateStore.sol
+++ contracts/CertificateStore.sol
@@ -9,6 +9,11 @@ contract CertificateStore is HasNoEther {
   string public name;
 
   event BatchIssued(bytes32 indexed batchRoot);
+  event ClaimRevoked(
+    bytes32 indexed claim,
+    bytes32 indexed batchRoot,
+    uint indexed revocationReason
+  );
 
   /// A recovation
   ///
@@ -16,7 +21,7 @@ contract CertificateStore is HasNoEther {
   /// for the details needed
   struct Revocation {
     // Merkle root of the batch
-    bytes32 batchMerkleRoot;
+    bytes32 batchRoot;
     /// Block number of revocation
     uint blockNumber;
     uint revocationReason;
@@ -24,9 +29,8 @@ contract CertificateStore is HasNoEther {
 
   /// A mapping of the certificate batch merkle root to the block number that was issued
   mapping(bytes32 => uint) batchIssued;
-  /// A mapping of the hash of the certificate being revoked to a revocation struct
-  // mapping(bytes32 => Revocation) certificateRevoked;
-  mapping(bytes32 => bool) certificateRevoked;
+  /// A mapping of the hash of the claim being revoked to a revocation struct
+  mapping(bytes32 => Revocation) claimRevoked;
 
   // modifier onlyBatchIssued(bytes32 merkleRoot) {
   //   Batch storage batch = batchIssued[merkleRoot]
@@ -50,12 +54,11 @@ contract CertificateStore is HasNoEther {
     BatchIssued(batchRoot);
   }
 
-  function revokeCertificate(
-    bytes32 certificateHash
-  ) public onlyOwner returns (bool)
+  function getIssuedBlock(
+    bytes32 batchRoot
+  ) public onlyIssuedBatch(batchRoot) view returns (uint)
   {
-    certificateRevoked[certificateHash] = true;
-    return true;
+    return batchIssued[batchRoot];
   }
 
   function isBatchIssued(
@@ -65,7 +68,27 @@ contract CertificateStore is HasNoEther {
     return (batchIssued[batchRoot] != 0);
   }
 
-  function checkProof(
+  function revokeClaim(
+    bytes32 batchRoot,
+    bytes32 claim,
+    bytes32[] proof,
+    uint reason
+  ) public onlyOwner onlyIssuedBatch(batchRoot) onlyNotRevoked(claim) returns (bool)
+  {
+    require(verifyMerkleProof(proof, batchRoot, claim));
+    Revocation memory revocation = Revocation(batchRoot, block.number, reason);
+    claimRevoked[claim] = revocation;
+    ClaimRevoked(claim, batchRoot, reason);
+  }
+
+  function isRevoked(
+    bytes32 claim
+  ) public view returns (bool)
+  {
+    return claimRevoked[claim].blockNumber != 0;
+  }
+
+  function verifyClaim(
     bytes32 batchRoot,
     bytes32 claim,
     bytes32[] proof
@@ -74,31 +97,41 @@ contract CertificateStore is HasNoEther {
     if (!isBatchIssued(batchRoot)) {
       return false;
     }
-    return merkleProofInvalidated(proof, batchRoot, claim);
+    return verifyMerkleProof(proof, batchRoot, claim);
   }
 
-  // Modified implementation of MerkleProof from zepplin-solidity
-  function merkleProofInvalidated(
+  /// Modified implementation of MerkleProof from zepplin-solidity
+  /// Also checks that along the entire path, nothing has been revoked
+  function verifyMerkleProof(
     bytes32[] _proof,
     bytes32 _root,
     bytes32 _leaf
   ) private view returns (bool)
   {
+
+    if (isRevoked(_root) || isRevoked(_leaf)) {
+      return false;
+    }
+
     bytes32 proofElement;
     bytes32 computedHash = _leaf;
 
     for (uint256 i = 0; i < _proof.length; i ++) {
       proofElement = _proof[i];
 
+      if (isRevoked(proofElement)) {
+        return false;
+      }
+
       if (computedHash < proofElement) {
         computedHash = keccak256(computedHash, proofElement);
       } else {
         computedHash = keccak256(proofElement, computedHash);
       }
-    }
 
-    if (certificateRevoked[computedHash]) {
-      return false;
+      if (isRevoked(computedHash)) {
+        return false;
+      }
     }
 
     return computedHash == _root;
@@ -111,6 +144,11 @@ contract CertificateStore is HasNoEther {
 
   modifier onlyNotIssuedBatch(bytes32 batchRoot) {
     require(!isBatchIssued(batchRoot));
+    _;
+  }
+
+  modifier onlyNotRevoked(bytes32 claim) {
+    require(!isRevoked(claim));
     _;
   }
 }
