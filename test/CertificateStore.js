e0ebfa6af0774ef1c2736e7d313c0c0c18b288f7
yehjxraymond <yehjxraymond@gmail.com>
2018-02-19 10:46:05 +0800

    Simplify contract by moving batching to application layer (#6)
    * Simplify contract by moving batching to application layer
    
    * Fixed naming issues

--- test/CertificateStore.js
+++ test/CertificateStore.js
@@ -18,7 +18,8 @@ contract("CertificateStore", accounts => {
     );
   });
 
-  const issueBatch = batchMerkleRoot => instance.issueBatch(batchMerkleRoot);
+  const issueCertificate = certificateMerkleRoot =>
+    instance.issueCertificate(certificateMerkleRoot);
 
   describe("constructor", () => {
     it("should have correct name", async () => {
@@ -43,36 +44,35 @@ contract("CertificateStore", accounts => {
     });
   });
 
-  describe("issueBatch", () => {
-    it("should be able to issue a certificate batch", async () => {
-      const batchMerkleRoot =
+  describe("issueCertificate", () => {
+    it("should be able to issue a certificate", async () => {
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
-      const receipt = await issueBatch(batchMerkleRoot);
+      const receipt = await issueCertificate(certificateMerkleRoot);
 
       // FIXME: Use a utility helper to watch for event
       expect(receipt.logs[0].event).to.be.equal(
-        "BatchIssued",
-        "Batch issued event not emitted."
+        "CertificateIssued",
+        "Certificate issued event not emitted."
       );
-      expect(receipt.logs[0].args.batchRoot).to.be.equal(
-        batchMerkleRoot,
+      expect(receipt.logs[0].args.certificate).to.be.equal(
+        certificateMerkleRoot,
         "Incorrect event arguments emitted"
       );
 
-      const issued = await instance.isBatchIssued(batchMerkleRoot);
-      expect(issued, "Certificate batch is not issued").to.be.true;
+      const issued = await instance.isCertificateIssued(certificateMerkleRoot);
+      expect(issued, "Certificate is not issued").to.be.true;
     });
 
     it("should not allow duplicate issues", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
       // Check that reissue is rejected
-      expect(instance.issueBatch(batchMerkleRoot)).to.be.rejectedWith(
-        /revert/,
-        "Duplicate issue was not rejected"
-      );
+      await expect(
+        instance.issueCertificate(certificateMerkleRoot)
+      ).to.be.rejectedWith(/revert/, "Duplicate issue was not rejected");
     });
 
     it("only allows the owner to issue", async () => {
@@ -80,138 +80,130 @@ contract("CertificateStore", accounts => {
       const owner = await instance.owner();
       expect(nonOwner).to.not.be.equal(owner);
 
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
 
-      expect(
-        instance.issueBatch(batchMerkleRoot, { from: nonOwner })
+      await expect(
+        instance.issueCertificate(certificateMerkleRoot, { from: nonOwner })
       ).to.be.rejectedWith(/revert/);
     });
   });
 
   describe("getIssuedBlock", () => {
     it("returns the block number of issued batches", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
-      const blockNumber = await instance.getIssuedBlock(batchMerkleRoot);
+      const blockNumber = await instance.getIssuedBlock(certificateMerkleRoot);
       expect(blockNumber).to.be.bignumber.greaterThan(0);
     });
 
     it("errors on unissued batch", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
 
-      expect(instance.getIssuedBlock(batchMerkleRoot)).to.be.rejectedWith(
-        /revert/
-      );
+      // This test may fail on ganache-ui (works for ganache-cli)
+      await expect(
+        instance.getIssuedBlock(certificateMerkleRoot)
+      ).to.be.rejectedWith(/revert/);
     });
   });
 
-  describe("isBatchIssued", () => {
+  describe("isCertificateIssued", () => {
     it("should return true for issued batch", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
-      const issued = await instance.isBatchIssued(batchMerkleRoot);
+      const issued = await instance.isCertificateIssued(certificateMerkleRoot);
       expect(issued, "Certificate batch is not issued").to.be.true;
     });
 
     it("should return false for certificate batch not issued", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
 
-      const issued = await instance.isBatchIssued(batchMerkleRoot);
+      const issued = await instance.isCertificateIssued(certificateMerkleRoot);
       expect(issued, "Certificate batch is issued in error").to.be.false;
     });
   });
 
-  describe("revokeClaim", () => {
-    it("should allow the revocation of a valid and issued claim (leaf)", async () => {
-      const batchMerkleRoot =
+  describe("revokeCertificate", () => {
+    it("should allow the revocation of a valid and issued certificate", async () => {
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
       const certificateHash =
         "0x10327d7f904ee3ee0e69d592937be37a33692a78550bd100d635cdea2344e6c7";
 
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
-      const receipt = await instance.revokeClaim(
-        batchMerkleRoot,
-        certificateHash,
-        1337
-      );
+      const receipt = await instance.revokeCertificate(certificateHash, 1337);
 
       // FIXME: Use a utility helper to watch for event
-      expect(receipt.logs[0].event).to.be.equal("ClaimRevoked");
-      expect(receipt.logs[0].args.claim).to.be.equal(certificateHash);
-      expect(receipt.logs[0].args.batchRoot).to.be.equal(batchMerkleRoot);
+      expect(receipt.logs[0].event).to.be.equal("CertificateRevoked");
+      expect(receipt.logs[0].args.certificate).to.be.equal(certificateHash);
       expect(receipt.logs[0].args.revocationReason).bignumber.to.be.equal(1337);
     });
 
     it("should allow the revocation of an issued root", async () => {
-      const batchMerkleRoot =
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
-      const certificateHash = batchMerkleRoot;
+      const certificateHash = certificateMerkleRoot;
 
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
-      const receipt = await instance.revokeClaim(
-        batchMerkleRoot,
-        certificateHash,
-        1337
-      );
+      const receipt = await instance.revokeCertificate(certificateHash, 1337);
 
       // FIXME: Use a utility helper to watch for event
-      expect(receipt.logs[0].event).to.be.equal("ClaimRevoked");
-      expect(receipt.logs[0].args.claim).to.be.equal(certificateHash);
-      expect(receipt.logs[0].args.batchRoot).to.be.equal(batchMerkleRoot);
+      expect(receipt.logs[0].event).to.be.equal("CertificateRevoked");
+      expect(receipt.logs[0].args.certificate).to.be.equal(certificateHash);
       expect(receipt.logs[0].args.revocationReason).bignumber.to.be.equal(1337);
     });
 
-    it("should not allow repeated revocation of a valid and issued claim", async () => {
-      const batchMerkleRoot =
+    it("should not allow repeated revocation of a valid and issued certificate", async () => {
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
       const certificateHash =
         "0x10327d7f904ee3ee0e69d592937be37a33692a78550bd100d635cdea2344e6c7";
 
-      await issueBatch(batchMerkleRoot);
+      await issueCertificate(certificateMerkleRoot);
 
-      await instance.revokeClaim(batchMerkleRoot, certificateHash, 1337);
+      await instance.revokeCertificate(certificateHash, 1337);
 
-      expect(
-        instance.revokeClaim(batchMerkleRoot, certificateHash, 1337)
+      await expect(
+        instance.revokeCertificate(certificateHash, 1337)
       ).to.be.rejectedWith(/revert/);
     });
 
-    it("should not allow revocation of an unissued claim", async () => {
-      const batchMerkleRoot =
-        "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
+    it("should allow revocation of an unissued certificate", async () => {
       const certificateHash =
         "0x10327d7f904ee3ee0e69d592937be37a33692a78550bd100d635cdea2344e6c7";
 
-      expect(
-        instance.revokeClaim(batchMerkleRoot, certificateHash, 1337)
-      ).to.be.rejectedWith(/revert/);
+      const receipt = await instance.revokeCertificate(certificateHash, 1337);
+
+      // FIXME: Use a utility helper to watch for event
+      expect(receipt.logs[0].event).to.be.equal("CertificateRevoked");
+      expect(receipt.logs[0].args.certificate).to.be.equal(certificateHash);
+      expect(receipt.logs[0].args.revocationReason).bignumber.to.be.equal(1337);
     });
   });
 
   describe("isRevoked", () => {
-    it("returns true for revoked claims", async () => {
-      const batchMerkleRoot =
+    it("returns true for revoked certificates", async () => {
+      const certificateMerkleRoot =
         "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
       const certificateHash =
         "0x10327d7f904ee3ee0e69d592937be37a33692a78550bd100d635cdea2344e6c7";
 
-      await issueBatch(batchMerkleRoot);
-      await instance.revokeClaim(batchMerkleRoot, certificateHash, 1337);
+      await issueCertificate(certificateMerkleRoot);
+      await instance.revokeCertificate(certificateHash, 1337);
 
       const revoked = await instance.isRevoked(certificateHash);
       expect(revoked).to.be.true;
     });
 
-    it("returns true for non-revoked claims", async () => {
+    it("returns true for non-revoked certificates", async () => {
       "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6330";
 
       const certificateHash =
