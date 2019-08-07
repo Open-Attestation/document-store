08c84a6ade5744b958a33e86e429c4b328eee8a8
RJ <me@rjchow.com>
2019-08-07 16:55:51 +0800

    automate test

--- migrations/2_deploy_document_store.js
+++ migrations/2_deploy_document_store.js
@@ -1,4 +1,6 @@
 const DocumentStore = artifacts.require("./DocumentStore.sol");
+const { generateHashes } = require("../test/generateHashes");
+let contract;
 
 module.exports = deployer => {
   return deployer
@@ -6,6 +8,16 @@ module.exports = deployer => {
       return DocumentStore.new(
         "Government Technology Agency of Singapore (GovTech)"
       );
+    })
+    .then(contractInstance => {
+      contract = contractInstance;
+      return contract.issue(
+        "0x3a267813bea8120f55a7b9ca814c34dd89f237502544d7c75dfd709a659f6329"
+      );
+    })
+    .then(txReceipt => {
+      console.log(txReceipt);
+      return contract.bulkIssue(generateHashes(256));
     })
     .then(console.log);
 };
