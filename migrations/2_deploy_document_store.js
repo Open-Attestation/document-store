aa692c77d70f60e183b6920e9a421e854fd75ff3
RJ <me@rjchow.com>
2019-08-07 14:54:24 +0800

    migration

--- migrations/2_deploy_document_store.js
+++ migrations/2_deploy_document_store.js
@@ -1,8 +1,11 @@
 const DocumentStore = artifacts.require("./DocumentStore.sol");
 
 module.exports = deployer => {
-  deployer.deploy(
-    DocumentStore,
-    "Government Technology Agency of Singapore (GovTech)"
-  );
+  return deployer
+    .then(() => {
+      return DocumentStore.new(
+        "Government Technology Agency of Singapore (GovTech)"
+      );
+    })
+    .then(console.log);
 };
