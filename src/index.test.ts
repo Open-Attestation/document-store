84a280ce575f4cc99d1bb6123a35bcfdf43c6eaf
Raymond Yeh <ray@geek.sg>
2020-03-19 15:41:16 +0800

    fix: override to be in deploy function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,31 +1,30 @@
 import {providers} from "ethers";
 import {deploy, deployAndWait, connect} from "./index";
 import {DocumentStoreCreatorFactory} from "../types/ethers-contracts/DocumentStoreCreatorFactory";
-import {overwriteDocumentStoreCreatorAddress} from "./config";
 
 const provider = new providers.JsonRpcProvider();
 const signer = provider.getSigner();
 let account: string;
+let deployedDocumentStoreCreatorAddress: string;
 
 beforeAll(async () => {
   // Deploy an instance of DocumentStoreFactory on the new blockchain
   const factory = new DocumentStoreCreatorFactory(signer);
   const receipt = await factory.deploy();
-  const factoryAddress = receipt.address;
-  overwriteDocumentStoreCreatorAddress(factoryAddress);
+  deployedDocumentStoreCreatorAddress = receipt.address;
   account = await signer.getAddress();
 });
 
 describe("deploy", () => {
   it("deploys a new DocumentStore contract without waiting for confirmation", async () => {
-    const receipt = await deploy("My Store", signer);
+    const receipt = await deploy("My Store", signer, deployedDocumentStoreCreatorAddress);
     expect(receipt.from).toBe(account);
   });
 });
 
 describe("deployAndWait", () => {
   it("deploys a new DocumentStore contract", async () => {
-    const instance = await deployAndWait("My Store", signer);
+    const instance = await deployAndWait("My Store", signer, deployedDocumentStoreCreatorAddress);
     const owner = await instance.owner();
     expect(owner).toBe(account);
     const name = await instance.name();
@@ -35,7 +34,7 @@ describe("deployAndWait", () => {
 
 describe("connect", () => {
   it("connects to existing contract", async () => {
-    const {address} = await deployAndWait("My Store", signer);
+    const {address} = await deployAndWait("My Store", signer, deployedDocumentStoreCreatorAddress);
     const instance = await connect(address, signer);
     const owner = await instance.owner();
     expect(owner).toBe(account);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,22 @@ import {DocumentStoreFactory} from "../types/ethers-contracts/DocumentStoreFacto
 import {DocumentStoreCreatorFactory} from "../types/ethers-contracts/DocumentStoreCreatorFactory";
 import {getDocumentStoreCreatorAddress} from "./config";
 
-export const deploy = async (name: string, signer: Signer): Promise<ContractTransaction> => {
-  const chainId = (await signer.provider?.getNetwork())?.chainId || 1;
-  const factory = DocumentStoreCreatorFactory.connect(getDocumentStoreCreatorAddress(chainId), signer);
+export const deploy = async (
+  name: string,
+  signer: Signer,
+  documentStoreCreatorAddressOverride?: string
+): Promise<ContractTransaction> => {
+  let documentStoreCreatorFactoryAddress = documentStoreCreatorAddressOverride;
+  if (!documentStoreCreatorFactoryAddress) {
+    const chainId = (await signer.provider?.getNetwork())?.chainId;
+    documentStoreCreatorFactoryAddress = getDocumentStoreCreatorAddress(chainId);
+  }
+  const factory = DocumentStoreCreatorFactory.connect(documentStoreCreatorFactoryAddress, signer);
   return factory.deploy(name);
 };
 
-export const deployAndWait = async (name: string, signer: Signer) => {
-  const receipt = await (await deploy(name, signer)).wait();
+export const deployAndWait = async (name: string, signer: Signer, documentStoreCreatorAddressOverride?: string) => {
+  const receipt = await (await deploy(name, signer, documentStoreCreatorAddressOverride)).wait();
   if (!receipt.logs || !receipt.logs[0].address) throw new Error("Fail to detect deployed contract address");
   return DocumentStoreFactory.connect(receipt.logs![0].address, signer);
 };
