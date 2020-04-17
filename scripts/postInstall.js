1b10d93baaa8c4928a6ccb062bf9d29207f38549
Nebulis <nebounet@gmail.com>
2020-04-17 15:34:26 +0800

    fix: move devdep

---
+++ scripts/postInstall.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+const path = require("path");
+const {execSync} = require("child_process");
+
+const truffle = path.join(process.cwd(), "node_modules", ".bin", "truffle");
+if (fs.existsSync(truffle) && process.env.npm_config_production !== "true") {
+  console.log("Running truffle build");
+  execSync("npm run build:sol");
+} else {
+  console.log("Not running truffle");
+}
+
+const typechain = path.join(process.cwd(), "node_modules", ".bin", "typechain");
+if (fs.existsSync(typechain) && process.env.npm_config_production !== "true") {
+  console.log("Running typechain");
+  execSync("npm run typechain");
+} else {
+  console.log("Not running typechain");
+}
