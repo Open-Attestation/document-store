2498b9a448d737a2e039fdbdada33d9280589aac
Nebulis <nebounet@gmail.com>
2020-08-06 17:59:25 +0800

    feat: upgrade typechain to target ethers v5
    BREAKING CHANGE: upgrading to a new target ethers v5

--- contracts/ProxyFactory.sol
+++ contracts/ProxyFactory.sol
@@ -1,4 +1,4 @@
 // Importing OpenZeppelin's Proxy Factory here to build it
-pragma solidity 0.5.12;
+pragma solidity 0.5.16;
 
 import "@openzeppelin/upgrades/contracts/upgradeability/ProxyFactory.sol";
