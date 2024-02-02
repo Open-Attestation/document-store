beee520872ee7c75c388e51726a8396ba15656c9
Seth <hullogh@gmail.com>
2024-02-02 23:01:37 +0800

    feat: support batched document in issued check
    BREAKING CHANGE: New changes for Document Store v4
    
    (cherry picked from commit 9305fa5051df86840ecfcdc11078270cbd755a08)

--- contracts/DocumentStore.sol
+++ contracts/DocumentStore.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 import "./BaseDocumentStore.sol";
 import "./base/DocumentStoreAccessControl.sol";
@@ -13,6 +14,8 @@ import "./base/DocumentStoreAccessControl.sol";
  * @notice A contract for storing and revoking documents with access control
  */
 contract DocumentStore is BaseDocumentStore, DocumentStoreAccessControl {
+  using MerkleProof for bytes32[];
+
   /**
    * @notice Initialises the contract with a name and owner
    * @param _name The name of the contract
@@ -36,16 +39,16 @@ contract DocumentStore is BaseDocumentStore, DocumentStoreAccessControl {
    * @notice Issues a document
    * @param document The hash of the document to issue
    */
-  function issue(bytes32 document) public onlyRole(ISSUER_ROLE) onlyNotIssued(document) {
-    BaseDocumentStore._issue(document);
+  function issue(bytes32 documentRoot) public onlyRole(ISSUER_ROLE) onlyNotIssued(document) {
+    BaseDocumentStore._issue(documentRoot);
   }
 
   /**
    * @notice Issues multiple documents
    * @param documents The hashes of the documents to issue
    */
-  function bulkIssue(bytes32[] memory documents) public onlyRole(ISSUER_ROLE) {
-    BaseDocumentStore._bulkIssue(documents);
+  function bulkIssue(bytes32[] memory documentRoots) public onlyRole(ISSUER_ROLE) {
+    BaseDocumentStore._bulkIssue(documentRoots);
   }
 
   /**
@@ -63,5 +66,25 @@ contract DocumentStore is BaseDocumentStore, DocumentStoreAccessControl {
    */
   function bulkRevoke(bytes32[] memory documents) public onlyRole(REVOKER_ROLE) {
     return BaseDocumentStore._bulkRevoke(documents);
+  }
+
+  function isIssued(
+    bytes32 documentRoot,
+    bytes32 document,
+    bytes32[] memory proof
+  ) public view returns (bool) {
+    require(document != 0x0 && documentRoot != 0x0, "Invalid document");
+    if (documentRoot == document && proof.length == 0) {
+      return _isIssued(document) && !isRevoked(document);
+    }
+    return
+      _isIssued(documentRoot) &&
+      !isRevoked(documentRoot) &&
+      !isRevoked(document) &&
+      proof.verify(documentRoot, document);
+  }
+
+  function isIssued(bytes32 documentRoot) public view returns (bool) {
+    return isIssued(documentRoot, documentRoot, new bytes32[](0));
   }
 }
