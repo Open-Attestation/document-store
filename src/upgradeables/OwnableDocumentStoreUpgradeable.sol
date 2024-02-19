23a2d522041203e4d09d7bb279e1e310bc287ca6
Seth <hullogh@gmail.com>
2024-02-19 18:37:26 +0800

    feat: ownable document store upgradeable

---
+++ src/upgradeables/OwnableDocumentStoreUpgradeable.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.8.23 <0.9.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "../base/BaseOwnableDocumentStore.sol";
+
+contract OwnableDocumentStoreUpgradeable is UUPSUpgradeable, BaseOwnableDocumentStore {
+  constructor(string memory name_, string memory symbol_, address initAdmin) {
+    initialize(name_, symbol_, initAdmin);
+  }
+
+  function initialize(string memory name_, string memory symbol_, address initAdmin) public initializer {
+    __OwnableDocumentStore_init(name_, symbol_, initAdmin);
+  }
+
+  function _authorizeUpgrade(address) internal view virtual override onlyRole(DEFAULT_ADMIN_ROLE) {}
+}
